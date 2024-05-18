6702ca3e67ffa42d5f8a6e28f9b3e34b5bfca0ac
SirioBambino <bambino.sirio@gmail.com>
2024-05-18 20:54:00 +0100

    feat: Add script to toggle mobile nav menu open and closed

---
+++ assets/js/header-toggle-mobile-menu.js
@@ -0,0 +1,5 @@
+// Toggle class in site nav to open and close mobile menu
+function toggleMobileMenu() {
+  let siteNav = document.querySelector(".site-nav");
+  siteNav.classList.toggle("site-open-menu");
+}
